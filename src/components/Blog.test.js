ff1549bb09b3baf1bafb67123f156331aa16d1f5
Nitish Raju <nitishraju@outlook.com>
2021-01-06 16:42:07 -0500

    Solved Ex 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,28 +1,27 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
+const fakeUser = {
+  token: 'abcxyz',
+  username: 'fakeUser',
+  id: '5ff0dfef3603112937630a0b'
+}
+const fakeBlog = {
+  title: 'React Patterns',
+  author: 'Michael Chan',
+  url: 'https://reactpatterns.com/',
+  user: fakeUser,
+  likes: 4
+}
+const mockLikeHandler = jest.fn()
+const mockDeleteHandler = jest.fn()
+
 test('renders only title and author by default', () => {
-  const fakeUser = {
-    token: 'abcxyz',
-    username: 'fakeUser',
-    id: '5ff0dfef3603112937630a0b'
-  }
-  const blog = {
-    title: 'React Patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    user: fakeUser,
-    likes: 4
-  }
-
-  const mockLikeHandler = jest.fn()
-  const mockDeleteHandler = jest.fn()
-
   const component = render(
     <Blog
-      blog={blog}
+      blog={fakeBlog}
       user={fakeUser}
       likeHandler={mockLikeHandler}
       deleteHandler={mockDeleteHandler}
@@ -30,4 +29,21 @@ test('renders only title and author by default', () => {
   )
 
   expect(component.container).toHaveTextContent('React Patterns - Michael Chan')
+})
+
+test('additionally renders blog url and likes when expanded', () => {
+  const component = render(
+    <Blog
+      blog={fakeBlog}
+      user={fakeUser}
+      likeHandler={mockLikeHandler}
+      deleteHandler={mockDeleteHandler}
+    />
+  )
+
+  const button = component.getByText('show')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(`URL: ${fakeBlog.url}`)
+  expect(component.container).toHaveTextContent(`Likes: ${fakeBlog.likes}`)
 })
